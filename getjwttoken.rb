d512a2aa60e8eb6aa33301321b774afb88365fc8
Jose Longhi <jose.longhi@crengland.com>
2024-09-04 16:31:09 -0600

    fix formula tar

--- getjwttoken.rb
+++ getjwttoken.rb
@@ -3,7 +3,7 @@
 class Getjwttoken < Formula
     desc "A tool to securely retrieve JWT tokens"
     homepage "https://github.com/jplonghi/homebrew-tools"
-    url "https://github.com/jplonghi/homebrew-tools/archive/refs/tags/v1.0.tar.gz"
+    url "https://github.com/jplonghi/homebrew-tools/archive/refs/tags/v1.0.0.tar.gz"
     version "1.0.0"
     sha256 "cade549ae79c6fb94e6d1455a93c7dc433683c197dce7f190703d80fc362f8d7"
   
