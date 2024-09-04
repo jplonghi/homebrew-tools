04d3536ba6ec149f72b93aa62f4911fec279d430
Jose Longhi <jose.longhi@crengland.com>
2024-09-04 16:33:54 -0600

    fix checksum

--- getjwttoken.rb
+++ getjwttoken.rb
@@ -5,7 +5,7 @@ class Getjwttoken < Formula
     homepage "https://github.com/jplonghi/homebrew-tools"
     url "https://github.com/jplonghi/homebrew-tools/archive/refs/tags/v1.0.0.tar.gz"
     version "1.0.0"
-    sha256 "cade549ae79c6fb94e6d1455a93c7dc433683c197dce7f190703d80fc362f8d7"
+    sha256 "6e28c421b4ab448d19e6d529107dc8eccee91ad540d4df01d7920151ef14fe85"
   
     def install
       bin.install "getjwttoken"
