f359811ae6b28f3ddcd36a3668dddb2e18747cd4
Jose Longhi <jose.longhi@crengland.com>
2024-09-04 16:09:44 -0600

    fix formula

--- getjwttoken.rb
+++ getjwttoken.rb
@@ -3,9 +3,9 @@
 class Getjwttoken < Formula
     desc "A tool to securely manage and retrieve JWT tokens"
     homepage "https://github.com/jplonghi/homebrew-tools"
-    url "https://github.com/jplonghi/homebrew-tools/blob/main/getjwttoken"
+    url "https://github.com/jplonghi/homebrew-tools/archive/refs/tags/v1.0.tar.gz"
     version "1.0.0"
-    sha256 "55f49dc8525b786183efc53a1c626b1538f97b3bb039b4b6b0dcefb1847ce1ee"
+    sha256 "cade549ae79c6fb94e6d1455a93c7dc433683c197dce7f190703d80fc362f8d7"
   
     def install
       bin.install "getjwttoken"
